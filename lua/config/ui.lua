1ac17690cc72ba80b273770cf938a899a05b06f3
Xenos112 <acristyano22@gmail.com>
2025-03-27 10:00:57 +0000

    move the ui config of telescope to ui dir

---
+++ lua/config/ui.lua
@@ -0,0 +1,9 @@
+local pine_border_color = "#31748f"
+local pine_background_color = "#191724"
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = pine_border_color, bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = pine_border_color, bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = pine_border_color, bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = pine_border_color, bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = pine_background_color })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = pine_background_color })
--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -21,15 +21,6 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     opts = function()
-      local pine_border_color = "#31748f"
-      local pine_background_color = "#191724"
-      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = pine_border_color, bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = pine_border_color, bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = pine_border_color, bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = pine_border_color, bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = pine_background_color })
-      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = pine_background_color })
       return {
         defaults = {
           borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
