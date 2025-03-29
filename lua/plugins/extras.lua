d17677872469ba1b832c72ab11e1a9382db72062
Xenos112 <acristyano22@gmail.com>
2025-03-29 12:26:49 +0000

    return the alpha.nvim

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -43,5 +43,23 @@ return {
     dependencies = {
       "MunifTanjim/nui.nvim",
     }
+  },
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+      dashboard.section.header.val = {
+        "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+        "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+        "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+        "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+        "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+        "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+      }
+      alpha.setup(dashboard.config)
+    end
   }
 }
