7170ef7d1f7b5892dfdc29fad6638ae780679997
Xenos112 <acristyano22@gmail.com>
2025-04-29 21:30:07 +0000

    reconfigure keymaps and make them silent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,5 +11,5 @@ map("n", "<tab>", ":bn<CR>", { silent = true })
 map("n", "<S-tab>", ":bp<CR>", { silent = true })
 
 map("n", "<leader>x", ":bd<CR>", {})
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", {})
+map("v", "<S-j>", ":m '>+1<CR>gv=gv", { silent = true })
+map("v", "<S-k>", ":m '<-2<CR>gv=gv", { silent = true })
