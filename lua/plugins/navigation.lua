147a0e5ab7f9daf90d3494929c29c1af35893d18
Xenos112 <acristyano22@gmail.com>
2025-05-23 14:39:48 +0000

    general fixes

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,46 +1,47 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    keys = {
-      { "<leader>e", "<cmd>Oil<CR>" }
-    },
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    opts = function()
-      return {
-        defaults = {
-          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          layout_config = {
-            prompt_position = "bottom",
-            preview_width = 0.0,
-            preview_cutoff = 9999,
-          },
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		keys = {
+			{ "<leader>e", "<cmd>Oil<CR>" },
+		},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		opts = function()
+			return {
+				defaults = {
+					borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					layout_config = {
+						prompt_position = "bottom",
+						preview_width = 0.0,
+						preview_cutoff = 9999,
+					},
 
-          previewer = false,
-        },
-      }
-    end,
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<CR>", { silent = true } },
-      { "<leader>fw", "<cmd>Telescope live_grep<CR>",  { silent = true } },
-      { "<leader>fg", "<cmd>Telescope git_files<CR>",  { silent = true } },
-      { "<leader>fh", "<cmd>Telescope help_tags<CR>",  { silent = true } },
-    },
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    'ThePrimeagen/harpoon',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      { "<leader>h", ":lua require(\"harpoon.mark\").add_file()<CR>",        { silent = true } },
-      { "<leader>H", ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>", { silent = true } },
-    }
-  }
+					previewer = false,
+				},
+			}
+		end,
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<CR>", { silent = true } },
+			{ "<leader>fw", "<cmd>Telescope live_grep<CR>", { silent = true } },
+			{ "<leader>fg", "<cmd>Telescope git_files<CR>", { silent = true } },
+			{ "<leader>fh", "<cmd>Telescope help_tags<CR>", { silent = true } },
+			{ "<leader>fb", "<cmd>Telescope buffers<CR>", { silent = true } },
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"ThePrimeagen/harpoon",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>h", ':lua require("harpoon.mark").add_file()<CR>', { silent = true } },
+			{ "<leader>H", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { silent = true } },
+		},
+	},
 }
