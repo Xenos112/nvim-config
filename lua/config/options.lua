7ccde66797888983398617d72876a8e36006b166
Xenos112 <acristyano22@gmail.com>
2025-05-23 14:40:09 +0000

    improve error and warnings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,12 +9,12 @@ vim.opt.smarttab = true
 
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#3d59a1", fg = "#191724" })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "YankHighlight",
-      timeout = 200,
-    })
-  end,
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "YankHighlight",
+			timeout = 200,
+		})
+	end,
 })
 
 -- remove the ~ sumbol inthe the lines
@@ -22,3 +22,27 @@ vim.opt.fillchars:append({ eob = " " })
 
 vim.o.signcolumn = "yes"
 vim.o.numberwidth = 2
+
+-- Add this to your lua/config/lsp.lua or init.lua
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●", -- You can use any icon here
+		symbols = {
+			error = "", -- nerd-font icon
+			warn = "",
+			info = "",
+			hint = "",
+		},
+	},
+	signs = true, -- Show gutter signs
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+})
+
+-- Define custom gutter signs (requires nerd-font installed)
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
