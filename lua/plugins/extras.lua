3543c8ab7cbf14410a05fef12fd63ca4e4218142
Xenos112 <acristyano22@gmail.com>
2025-07-23 22:29:33 +0000

    remove codeium ai

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -45,18 +45,6 @@ return {
     end,
   },
   {
-    'Exafunction/windsurf.vim',
-    event = "BufEnter",
-    config = function()
-      vim.keymap.set('i', '<c-Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<Tab>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-        { expr = true, silent = true })
-      vim.keymap.set('i', '<s-Tab>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-        { expr = true, silent = true })
-      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-    end
-  },
-  {
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
       { 'tpope/vim-dadbod',                     lazy = true },
